8b81ccd219b2280fbe1fa12f0e604275a1e76f69
Cullen Knott <cullenp.knott@gmail.com>
2023-08-31 11:56:32 -0700

    added logic to add content and read content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // create connection to database and what version to use
+  const jateDb = await openDB('jate', 1);
+  //create new transaction and specify the database and privileges to use
+  const tx = jateDb.transaction('jate', 'readwrite');
+  //open the object store
+  const store = tx.objectStore('jate');
+  //update content to the database
+  const request = store.put({ id: 1, value: content });
+  // confirm the request
+  const result = await request;
+  console.log('Data saved to the databse', result)
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // create connection to database and what version to use
+  const jateDb = await openDB('jate', 1);
+  // create a new transaction, specify the database and privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+  // open desired object store
+  const store = tx.objectStore('jate');
+  // use getAll() method to get all the data in the database,
+  const request = store.getAll();
+  // confirm the request.
+  const result = await request;
+  console.log('Data read from database', result);
+  return result.value;
+};
 
 initdb();
